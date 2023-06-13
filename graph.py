11dfbb0006edc1e75094b55873d1ccdc556b93fb
andrescarcia <49753315+andrescarcia@users.noreply.github.com>
2023-06-12 23:44:28 -0400

    Update on graph

--- graph.py
+++ graph.py
@@ -37,8 +37,14 @@ class Graph:
 
 
 ###
-##decidí usar heapq porque es una biblioteca de Python que proporciona una implementación eficiente de una cola de prioridad. En el algoritmo de Dijkstra, necesitamos una estructura de datos que nos permita almacenar y recuperar los nodos con el menor costo acumulado de manera eficiente. La cola de prioridad es una estructura de datos adecuada para este propósito.
-##La biblioteca heapq utiliza una estructura de montículo (heap) binario, que es una estructura de árbol binario en la que cada nodo es menor (o mayor, dependiendo de si es un montículo mínimo o máximo) que sus hijos. Esto permite encontrar y extraer el elemento mínimo (o máximo) en tiempo O(log n), donde n es el número de elementos en el montículo.
-##En nuestro caso, utilizamos heapq.heappush() para agregar elementos a la cola de prioridad y heapq.heappop() para extraer el elemento con el menor costo acumulado. Estas operaciones tienen una complejidad de tiempo O(log n), lo que hace que el algoritmo de Dijkstra sea más eficiente en comparación con el uso de una lista simple, donde encontrar y extraer el elemento mínimo tendría una complejidad de tiempo O(n).
+##decidí usar heapq porque es una biblioteca de Python que proporciona una implementación eficiente de una cola de prioridad. En el algoritmo de Dijkstra,
+#  necesitamos una estructura de datos que nos permita almacenar y recuperar los nodos con el menor costo acumulado de manera eficiente. La cola de prioridad es una
+#  estructura de datos adecuada para este propósito.
+##La biblioteca heapq utiliza una estructura de montículo (heap) binario, que es una estructura de árbol binario en la que cada nodo es menor (o mayor, dependiendo 
+# de si es un montículo mínimo o máximo) que sus hijos. Esto permite encontrar y extraer el elemento mínimo (o máximo) en tiempo O(log n), donde n es el número de 
+# elementos en el montículo.
+##En nuestro caso, utilizamos heapq.heappush() para agregar elementos a la cola de prioridad y heapq.heappop() para extraer el elemento con el menor costo acumulado.
+#  Estas operaciones tienen una complejidad de tiempo O(log n), lo que hace que el algoritmo de Dijkstra sea más eficiente en comparación con el uso de una lista simple, 
+# donde encontrar y extraer el elemento mínimo tendría una complejidad de tiempo O(n).
 ##En resumen, elegí heapq porque proporciona una implementación eficiente de una cola de prioridad, lo que mejora la eficiencia del algoritmo de Dijkstra
 ###
